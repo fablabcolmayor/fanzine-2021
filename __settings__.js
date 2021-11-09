4e92fe2bba5ff4bc661af5942cbeec22cc3a622a
moranauta <manuel.morales@colmayor.edu.co>
2021-11-09 12:27:55 -0500

    1

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1268343.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 59820323, 59820351, 59820349, 59820326, 59820336, 59820338, 59820318, 59820322, 59821255 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/59950209/1/basis.wasm.js', 'wasmUrl' : 'files/assets/59950210/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/59950208/1/basis.js', 'preload' : false},
+];
